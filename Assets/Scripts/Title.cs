32a3bb7de3ff0a966e6364f2fc8a604da1c02a19
Cyclops1970 <37245822+Cyclops1970@users.noreply.github.com>
2018-10-06 15:08:44 +0800

    60 levels

--- Assets/Scripts/Title.cs
+++ Assets/Scripts/Title.cs
@@ -2,28 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+using UnityEngine.UI;
 
 public class Title : MonoBehaviour
 {
 
-    public GameObject background;
+    //public GameObject background;
 
     // Use this for initialization
     void Start()
     {
-        DisplayTitle();
+        //DisplayTitle();
         StartCoroutine(TitleToHome());
     }
 
     IEnumerator TitleToHome()
     {
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(5);
         SceneManager.LoadScene("Home");
 
     }
 
     void DisplayTitle()
     {
+        //Add VideoPlayer to the GameObject
+        //videoPlayer = gameObject.AddComponent<VideoPlayer>();
+        //Add AudioSource
+        //audioSource = gameObject.AddComponent<AudioSource>();
+
+        //Set video To Play then prepare Audio to prevent Buffering
+        //videoPlayer.clip = movie;
+
+        ///videoPlayer.source = VideoSource.VideoClip;
+        
+        //Assign the Texture from Video to RawImage to be displayed
+        //image.texture = videoPlayer.texture;
+
+        //Play Video
+        //videoPlayer.Play();
+
+        //Play Sound
+        //audioSource.Play();
+
+        /*
         SpriteRenderer bkg = background.GetComponent<SpriteRenderer>();
         float camY = Camera.main.orthographicSize * 2;
         float camX = Camera.main.orthographicSize * 2 * Screen.width / Screen.height; // covers different aspectc ratios
@@ -36,6 +58,6 @@ public class Title : MonoBehaviour
 
         bkg.transform.position = Vector2.zero; // Optional, place in middle
         bkg.transform.localScale = scale;
-
+        */
     }
 }
